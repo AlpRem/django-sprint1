17a63fcadd5ac4f531bc0d77775267aacfaf7508
AlpRem <test@me.comm>
2026-05-31 14:18:01 +0300

    fix PEP8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -47,7 +47,7 @@ posts = [
 # Create your views here.
 def index(request):
     template = 'blog/index.html'
-    context = {'posts':  list(reversed(posts))}
+    context = {'posts': list(reversed(posts))}
     return render(request, template, context)
 
 
